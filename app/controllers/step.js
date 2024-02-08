a00c45f138a79283e43d26cc55d38eafe47210b2
snowflux <vuhoangminhtrung305@gmail.com>
2024-02-08 17:33:26 +0700

    fafa

--- app/controllers/step.js
+++ app/controllers/step.js
@@ -1,3 +1,8 @@
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-app.js"
+import { getStorage  } from 'https://www.gstatic.com/firebasejs/10.7.2/firebase-storage.js';
+
+
 let stepsData;
 let stateData;
 // note: Insert 
@@ -382,4 +387,22 @@ function handleUpdateSteps() {
 document.addEventListener('DOMContentLoaded', function () {
     handleUpdateSteps()
 });
+
+function saveImageFirebase(){
+
+    
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBXGDpGLkoxPHaMGIG-E6GxviDDssv-97c",
+  authDomain: "thesis-268ea.firebaseapp.com",
+  projectId: "thesis-268ea",
+  storageBucket: "thesis-268ea.appspot.com",
+  messagingSenderId: "1065392850994",
+  appId: "1:1065392850994:web:023a10aca1806650fd142b",
+  measurementId: "G-XHJ0ES966N"
+};
+
+const app = initializeApp(firebaseConfig);
+const storage = app.storage();
+}
 
